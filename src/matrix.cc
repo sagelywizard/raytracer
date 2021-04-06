7824cf2ffd9bfe5a6e29c64db6c5f2f8b754f546
Benjamin Bastian <benjamin.bastian@gmail.com>
2021-04-05 19:20:12 -0700

    remove commented out code

--- src/matrix.cc
+++ src/matrix.cc
@@ -7,18 +7,6 @@ void Matrix<R, C>::set(int row, int column, float value) {
   data_[row * R + column] = value;
 }
 
-// template <size_t R, size_t C>
-// bool Matrix<R, C>::operator==(const Matrix<R, C>& other) const {
-//  for (size_t row = 0; row < R; row++) {
-//    for (size_t column = 0; column < C; column++) {
-//      if (data_[row * R + column] != other.data_[row * R + column]) {
-//        return false;
-//      }
-//    }
-//  }
-//  return true;
-//}
-
 template <size_t R, size_t C>
 bool operator==(const Matrix<R, C>& matrix_a, const Matrix<R, C>& matrix_b) {
   for (size_t row = 0; row < R; row++) {
